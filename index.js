ad94d0be8b85a02209eb633d3cf0c5775b17f9e0
Teni Omole <tennyomole@yahoo.com>
2023-09-24 20:24:07 +0100

    Update to replace express-graphql with graphql-http

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express")
-var { graphqlHTTP } = require("express-graphql")
-var { buildSchema } = require("graphql")
-
+var {createHandler} = require ("graphql-http/lib/use/express")
+var { GraphQLSchema,GraphQLList, GraphQLObjectType, GraphQLInt, GraphQLString } = require("graphql")
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default
 // Construct a schema, using GraphQL schema language
 var feed_array = [{
         id: 1,
@@ -38,80 +39,107 @@ var suggested_connections_array = [{
     },
 ]
 
+var feedType = new GraphQLObjectType({ 
+    name: "Feed",
+    fields: {
+       id: { type: GraphQLInt },
+       title: { type:GraphQLString },
+       content: { type:GraphQLString }
+    }
+});
 
-var schema = buildSchema(`
-type Feed {
-      id: Int,
-      title: String,
-      content: String
-  }
-type Page {
-    id: Int
-    title: String
 
-}
-input PageInput {
-     title: String
- }
-input FeedInput {
-     title : String
-     content: String
- }
- 
-type SuggestedConnection {
-    id: Int
-    name: String
-}
+var pageType = new GraphQLObjectType({ 
+    name: "Page",
+    fields: {
+       id: { type: GraphQLInt },
+       title: { type:GraphQLString },
+    }
+});
 
-  type Query {
-   
-    feed: [Feed],
-    page: [Page],
-    suggested_connection: [SuggestedConnection]
 
-  }
+var SuggestedConnectionType = new GraphQLObjectType({ 
+    name: "SuggestedConnection",
+    fields: {
+       id: { type: GraphQLInt },
+       name: { type:GraphQLString },
+    }
+});
 
-  type Mutation {
-     addPage (input: PageInput) : Page ,
-     addFeed(input: FeedInput) : Feed
-  }
+var schema = new GraphQLSchema ({
 
- 
-
-`)
-
-// The root provides a resolver function for each API endpoint
-var root = {
-
-    feed: () => {
-        return feed_array
-    },
-    page: () => {
-        return pages_array
-    },
-    suggested_connection: () => {
-        return suggested_connections_array
-    },
-    addPage: (x) => {
-        let new_object = { id: pages_array.length + 1, title: x.input.title }
+  mutation: new GraphQLObjectType({ 
+     name: 'Mutation',
+     fields: {
+        addPage: {
+           type: pageType,
+           args:{ title: {
+             type: GraphQLString 
+           } },
+           resolve:  (_, {title}) => {
+        let new_object = { id: pages_array.length + 1, title: title }
         pages_array.push(new_object)
         return new_object;
-    },
-    addFeed: (x) => {
-        let new_object = { id: feed_array.length + 1, title: x.input.title, content: x.input.content }
+    }
+        } ,
+
+      addFeed: {
+
+           type: feedType,
+           args:{ title: {
+             type: GraphQLString 
+           },
+             content: {
+             type: GraphQLString 
+            }
+            },
+            resolve: (_, {title,content}) => {
+
+        let new_object = { id: feed_array.length + 1, title: title, content: content }
         feed_array.push(new_object)
         return new_object;
+
+  }
+      }
+     }
+  }),
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+       feed: {type: new GraphQLList(feedType), resolve:  () => {
+        return feed_array
     }
-}
+  } ,
+      page: {
+          type: new GraphQLList(pageType),
+          resolve: () => {
+        return pages_array
+    }
+
+      },
+     suggested_connection: {
+
+          type: new GraphQLList(SuggestedConnectionType),
+          resolve: () => {
+        return suggested_connections_array
+
+    }
+     }
+ }
+})
+});
+ 
+
+ 
+
 
 var app = express()
 app.use(
     "/graphql",
-    graphqlHTTP({
-        schema: schema,
-        rootValue: root,
-        graphiql: true,
+    createHandler({
+        schema
     })
 )
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 app.listen(5000)
-console.log("Running a GraphQL API server at http://localhost:5000/graphql")
+console.log("Running a GraphQL API server at http://localhost:5000/graphql\nPlayground is running at http://localhost:5000/playground")
