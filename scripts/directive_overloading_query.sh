dad3ce9dbefb82788d1c6e6c19db0e3dc891065a
teni <tennyomole@yahoo.com>
2024-04-11 21:24:26 +0100

    Edit Directive Query

--- scripts/directive_overloading_query.sh
+++ scripts/directive_overloading_query.sh
@@ -1,3 +1,3 @@
-count = ${ $1:+5}
-overload=$(for i in $(seq 1 $count); do echo -n @a  ; done ;)
+overload=$(for i in $(seq 1 $1); do echo -n @a  ; done ;)
 curl 'http://localhost:5013/graphql' -X POST  -H "Content-Type: application/json" --data-raw "{\"query\":\"query { __typename    $overload }\" }"
+
