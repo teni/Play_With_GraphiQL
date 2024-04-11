7033e5159903c4f06e86be32855ad8e677164553
teni <tennyomole@yahoo.com>
2024-04-11 22:41:49 +0100

    Alias Based Batch and Password File

---
+++ scripts/alias_based_query_batching.sh
@@ -0,0 +1,11 @@
+input_file="password_file"
+i=0;
+alias_part="";
+while IFS= read -r line
+do
+  line_array=($line);
+  ((i+=1));
+  alias_part=$alias_part" alias$i : login(username: \"${line_array[0]}\", password: \"${line_array[1]}\") {accessToken }" ;
+done < "$input_file";
+
+echo curl 'http://localhost:5013/graphql' -X POST  -H "Content-Type: application/json" --data-raw "{\"query\":\"mutation { $alias_part  }\" }"
