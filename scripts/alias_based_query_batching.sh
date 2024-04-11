2518ac71e07311f3b1a5c713cc9e1e0f0999e03b
teni <tennyomole@yahoo.com>
2024-04-11 23:50:22 +0100

    Fix Alias Script Bug

--- scripts/alias_based_query_batching.sh
+++ scripts/alias_based_query_batching.sh
@@ -2,7 +2,7 @@
 
 input_file="password_file"
 i=0;
-alias_part="";
+alias_part="mutation {";
 while IFS= read -r line
 do
   line_array=($line);
@@ -10,6 +10,6 @@ do
   alias_part=$alias_part" alias$i : login(username: '${line_array[0]}', password: '${line_array[1]}') {accessToken } " ;
 done < "$input_file";
 
+alias_part= $alias_part"}"
 
-echo $alias_part;
-curl 'http://localhost:5013/graphql' -X POST  -H "Content-Type: application/json" --data-raw "{\"query\":\" mutation { $alias_part  }\" }"
+curl 'http://localhost:5013/graphql' -X POST  -H "Content-Type: application/json" --data-raw "{\"query\":\"  $alias_part   \" }"
