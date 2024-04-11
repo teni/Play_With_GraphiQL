b79328a88206f4832660cc5878b703a78cdb101e
teni <tennyomole@yahoo.com>
2024-04-12 00:27:25 +0100

    Fix Alias Script Bug

--- scripts/alias_based_query_batching.sh
+++ scripts/alias_based_query_batching.sh
@@ -10,6 +10,6 @@ do
   alias_part=$alias_part" alias$i : login(username: \"${line_array[0]}\", password: \"${line_array[1]}\") {accessToken } " ;
 done < "$input_file";
 
-alias_part= $alias_part"}"
+alias_part=$alias_part"}"
 
 curl 'http://localhost:5013/graphql' -X POST  -H "Content-Type: application/json" --data-raw "{\"query\":\"  $alias_part   \" }"
